69923978bf1fdfe94afa0e9e53a465afae807176
Joshua Taylor <joshuasammytaylor@gmail.com>
2024-11-27 14:30:13 +0000

    Added new functionality

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,15 +1,34 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	PostRepository postrepository;
+
 	@RequestMapping(value = "/")
 	public RedirectView index() {
 		return new RedirectView("/posts");
 	}
 
-
+	@GetMapping("/showallposts")
+	public ModelAndView showallposts() {
+		ModelAndView modelAndView = new ModelAndView("/showallposts");
+		List<Post> posts = postrepository.findAll();
+		modelAndView.addObject("posts", posts);
+		return modelAndView;
+    }
 }
--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-    @Query("SELECT p FROM Post p WHERE p.user_id = :userId")
-    Iterable<Post> findAllByUserId(@Param("userId") Long userId);
+    @Override
+    List<Post> findAll();
 }
