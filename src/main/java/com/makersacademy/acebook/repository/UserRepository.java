a991652aeb67ccae3186a26770d6da4b15e36ff7
Matt Rowell <matthewjrowell@icloud.com>
2024-11-27 13:36:20 +0000

    Implemented Auth0 into postgresql database

--- src/main/java/com/makersacademy/acebook/repository/UserRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     public Optional<User> findUserByUsername(String username);
+    public Optional<User> findUserByEmail(String email);
+    public Optional<User> findByAuth0Id(String auth0Id);
 }
