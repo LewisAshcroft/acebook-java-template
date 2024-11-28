c79e6c0951dc0c08b92194ecfc1c39ac5a7fa690
Isaac Spiers <isaacspiers@MASTUDENT-Isaac-Spiers-C17GMB2AQ6L4.local>
2024-11-28 11:23:55 +0000

    Pull posts by user id

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +18,8 @@ import java.util.Optional;
 public class UsersController {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
 
     @GetMapping("/users/after-login")
     public RedirectView afterLogin() {
@@ -35,8 +39,10 @@ public class UsersController {
     @GetMapping("/profile/{id}")
     public ModelAndView userProfile(@PathVariable Long id, Model model) {
         ModelAndView modelAndView = new ModelAndView("/user/profile");
-        User user = userRepository.findById(1L).get();
+        User user = userRepository.findById(id).get();
         modelAndView.addObject("user", user);
+        Iterable<Post> posts = postRepository.findAllByUserId(id);
+        modelAndView.addObject("posts", posts);
         return modelAndView;
     }
 }
